5e713d4f2c0726de55b8f15897c3d85071a2503a
elgasing23 <abinaufal901@gmail.com>
2026-02-20 19:50:57 +0700

    initial commit

---
+++ src/main/java/com/example/bidmartbooking/BidmartBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.bidmartbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidmartBookingApplication.class, args);
+    }
+
+}
