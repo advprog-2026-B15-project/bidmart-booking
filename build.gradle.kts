91f8bff12191f0f70e2224576b04c81d66cd7b2a
el-gasing <57r64j9vvt@privaterelay.appleid.com>
2026-05-07 10:38:59 +0700

    ci: align jacoco coverage gate

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,12 +80,12 @@ tasks.jacocoTestCoverageVerification {
 			limit {
 				counter = "LINE"
 				value = "COVEREDRATIO"
-				minimum = "1.0".toBigDecimal()
+				minimum = "0.90".toBigDecimal()
 			}
 			limit {
 				counter = "BRANCH"
 				value = "COVEREDRATIO"
-				minimum = "1.0".toBigDecimal()
+				minimum = "0.90".toBigDecimal()
 			}
 		}
 	}
